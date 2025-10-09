b44beea9d62ab4dc0e62296a4466fa4d535a8320
Jazz Khanuja <90267455+NeuralNirvana@users.noreply.github.com>
2025-10-09 12:22:22 +0530

    Add files via upload

---
+++ day3code1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+
+    // Taking input from the user
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    // Converting Celsius to Fahrenheit
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    // Displaying result
+    printf("Temperature in Fahrenheit = %.2f\n", fahrenheit);
+
+    return 0;
+}
